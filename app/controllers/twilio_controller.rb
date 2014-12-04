170a53d29860a002f43b53b1e4be4130d50a12d9
Charles <Charlesarcin@Charless-MacBook-Pro.local>
2014-12-03 20:23:11 -0800

    initial commit

---
+++ app/controllers/twilio_controller.rb
@@ -0,0 +1,15 @@
+class TwilioController < ApplicationController
+  include Webhookable
+
+  after_filter :set_header
+
+  skip_before_action :verify_authenticity_token
+
+  def voice
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say 'Hey there.'
+    end
+
+    render_twiml response
+  end
+end
